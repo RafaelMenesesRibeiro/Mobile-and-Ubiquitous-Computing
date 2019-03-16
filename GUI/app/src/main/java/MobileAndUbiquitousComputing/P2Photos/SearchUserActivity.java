4f4c8441f1324da428c975312d542a9e93088f53
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-03-16 18:38:31 +0000

    SearchUserActivity: OK

--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/SearchUserActivity.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/SearchUserActivity.java
@@ -3,13 +3,11 @@ package MobileAndUbiquitousComputing.P2Photos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import MobileAndUbiquitousComputing.P2Photos.R;
-
-public class NewAlbumActivity extends AppCompatActivity {
+public class SearchUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_album);
+        setContentView(R.layout.activity_search_user);
     }
 }
