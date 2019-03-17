1a3b908709449493ace988239fa5ce1358e04635
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-03-17 21:59:47 +0000

    Base functionality for Login page
    - Only enables button click for Login Button and Sign up Button if all the required fiels are not empy.
    - Disables the buttons otherwise.
    - Simulates account login and changes screen to the Main Menu

---
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/Helpers/Login.java
@@ -0,0 +1,30 @@
+package MobileAndUbiquitousComputing.P2Photos.Helpers;
+
+import android.support.annotation.NonNull;
+
+public class Login {
+    private static String SessionID;
+    private static String username;
+
+    private static String password;
+
+    public static String getSessionID() { return SessionID; }
+    public static String getUsername() { return username; }
+    public static String getPassword() { return password; }
+
+    public static void setSessionID(String sessionID) { SessionID = sessionID; }
+    public static void setUsername(String username) { Login.username = username; }
+    public static void setPassword(String password) { Login.password = password; }
+
+    public Login(String username, String password) {
+        Login.username = username;
+        Login.password = password;
+    }
+
+    public String LoginUser() {
+        // TODO - Implement communication with server for new Session ID. //
+        Login.SessionID = "sessionID";
+        return Login.SessionID;
+    }
+
+}
--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/LoginActivity.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/LoginActivity.java
@@ -1,8 +1,15 @@
 package MobileAndUbiquitousComputing.P2Photos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import MobileAndUbiquitousComputing.P2Photos.Helpers.Login;
 import MobileAndUbiquitousComputing.P2Photos.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -11,5 +18,56 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        final Button loginButton = findViewById(R.id.LoginButton);
+        final Button signupButton = findViewById(R.id.SignUpBottom);
+        final EditText usernameInput = findViewById(R.id.usernameInputBox);
+        final EditText passwordInput = findViewById(R.id.passwordInputBox);
+
+        usernameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { /* Do nothing. */ }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ActivateButtons(usernameInput, passwordInput, loginButton, signupButton);
+            }
+            @Override
+            public void afterTextChanged(Editable s) { /* Do nothing */ }
+        });
+
+        passwordInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { /* Do nothing. */ }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ActivateButtons(usernameInput, passwordInput, loginButton, signupButton);
+            }
+            @Override
+            public void afterTextChanged(Editable s) { /* Do nothing. */ }
+        });
+    }
+
+    private void ActivateButtons(EditText usernameInput, EditText passwordInput, Button loginButton, Button signupButton) {
+        if (!usernameInput.getText().toString().isEmpty() && !passwordInput.getText().toString().isEmpty()) {
+            loginButton.setClickable(true);
+            signupButton.setClickable(true);
+        }
+        else {
+            loginButton.setClickable(false);
+            signupButton.setClickable(false);
+        }
+    }
+
+    public void LoginClicked(View view) {
+        String username = ((EditText) findViewById(R.id.usernameInputBox)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passwordInputBox)).getText().toString();
+        Login login = new Login(username, password);
+        login.LoginUser();
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
+    }
+
+    public void SignUpClicked(View view) {
+        // TODO - Implement this. //
     }
 }
