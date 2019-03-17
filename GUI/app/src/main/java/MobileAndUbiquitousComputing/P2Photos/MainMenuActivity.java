e544fc308aff428944c527fff95f36c276398fd4
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-03-17 00:19:18 +0000

    ViewAlbumActivity: Bugged

--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/MainMenuActivity.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/MainMenuActivity.java
@@ -1,7 +1,9 @@
 package MobileAndUbiquitousComputing.P2Photos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void viewAlbumClicked(View view) {
+        Intent intent = new Intent(this, AlbumViewActivity.class);
+        startActivity(intent);
     }
 }
