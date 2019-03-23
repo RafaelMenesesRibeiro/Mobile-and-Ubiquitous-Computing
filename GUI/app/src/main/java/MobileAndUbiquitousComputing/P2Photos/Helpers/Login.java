a618c9627f67f59da64a7d2304f0c4c59d3e5abd
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-03-23 17:34:06 +0000

    Implemented communication with the server for Login and Sign up

--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/Helpers/Login.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/Helpers/Login.java
@@ -1,5 +1,25 @@
 package MobileAndUbiquitousComputing.P2Photos.Helpers;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+import MobileAndUbiquitousComputing.P2Photos.DataObjects.PostRequestData;
+import MobileAndUbiquitousComputing.P2Photos.DataObjects.RequestData;
+import MobileAndUbiquitousComputing.P2Photos.DataObjects.ResponseData;
+
 public class Login {
     private static String SessionID;
     private static String username;
@@ -15,12 +35,43 @@ public class Login {
     public static void setPassword(String password) { Login.password = password; }
 
     public Login(String username, String password) {
+        Log.i("MSG", "Logging in as " + username + ".");
+        String url = "http://p2photo-production.herokuapp.com/login";
+        JSONObject json = new JSONObject();
+        try {
+            // TODO - Fetch these properties. //
+            json.accumulate("username", "monkey12");
+            json.accumulate("password", "macaco90");
+        }
+        catch (JSONException jex) {
+            jex.printStackTrace();
+        }
+        RequestData rData = new PostRequestData(RequestData.RequestType.POST, url, json);
+        try {
+            ResponseData result = new ExecuteQuery().execute(rData).get();
+            int code = result.getServerCode();
+            if (code == 200) {
+                Log.i("STATUS", "The login operation was successful");
+            }
+            else if (code == 401) {
+                Log.i("STATUS", "The login operation was unsuccessful. The username or password are incorrect.");
+            }
+            else {
+                Log.i("STATUS", "The login operation was unsuccessful. Unknown error.");
+            }
+        }
+        catch (ExecutionException eex) {
+            eex.printStackTrace();
+        }
+        catch (InterruptedException iex) {
+            iex.printStackTrace();
+        }
+
         Login.username = username;
         Login.password = password;
     }
 
     public String LoginUser() {
-        // TODO - Implement communication with server for new Session ID. //
         Login.SessionID = "sessionID";
         return Login.SessionID;
     }
--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/Helpers/Signup.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/Helpers/Signup.java
@@ -1,10 +1,52 @@
 package MobileAndUbiquitousComputing.P2Photos.Helpers;
 
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+
+import MobileAndUbiquitousComputing.P2Photos.DataObjects.PostRequestData;
+import MobileAndUbiquitousComputing.P2Photos.DataObjects.RequestData;
+import MobileAndUbiquitousComputing.P2Photos.DataObjects.ResponseData;
+
 public class Signup {
 
     public static boolean SignupUser(String username, String password) {
+        Log.i("MSG", "Signing up in as " + username + ".");
+        String url = "http://p2photo-production.herokuapp.com/signUp";
+        JSONObject json = new JSONObject();
+        try {
+            // TODO - Fetch these properties. //
+            json.accumulate("username", "monkey12");
+            json.accumulate("password", "macaco90");
+        }
+        catch (JSONException jex) {
+            jex.printStackTrace();
+        }
+        RequestData rData = new PostRequestData(RequestData.RequestType.POST, url, json);
+        try {
+            ResponseData result = new ExecuteQuery().execute(rData).get();
+            int code = result.getServerCode();
+            if (code == 200) {
+                Log.i("STATUS", "The sign up operation was successful");
+            }
+            else if (code == 422) {
+                Log.i("STATUS", "The sign up operation was unsuccessful. The username chosen already exists.");
+            }
+            else {
+                Log.i("STATUS", "The sign up operation was unsuccessful. Unknown error.");
+            }
+        }
+        catch (ExecutionException eex) {
+            eex.printStackTrace();
+        }
+        catch (InterruptedException iex) {
+            iex.printStackTrace();
+        }
+
         Login login = new Login(username, password);
-        // TODO - Implement communication with server
         String sessionID = "sessionID";
         login.LoginUser(sessionID);
         // Returns true if successful, false otherwise.
