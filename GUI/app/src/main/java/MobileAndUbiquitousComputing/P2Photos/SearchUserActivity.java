2cba17ae048e29065fc2320e3c73fde3b5115653
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-03-27 10:36:19 +0000

    minor fixes

--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/SearchUserActivity.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/SearchUserActivity.java
@@ -2,6 +2,7 @@ package MobileAndUbiquitousComputing.P2Photos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SearchUserActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class SearchUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_user);
+    }
+
+    public void SearchUser(View view) {
+
     }
 }
