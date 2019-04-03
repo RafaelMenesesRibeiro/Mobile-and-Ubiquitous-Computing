a674ff79938a95e469c045c62703d58463cf1e20
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-04 00:19:40 +0100

    Create HashMapArrayAdapter.java

---
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/Helpers/HashMapArrayAdapter.java
@@ -0,0 +1,45 @@
+package MobileAndUbiquitousComputing.P2Photos.Helpers;
+
+import java.util.List;
+import java.util.Map;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+@SuppressWarnings("ALL")
+class HashMapArrayAdapter extends ArrayAdapter {
+
+    private static class ViewHolder {
+        TextView keyText;
+        TextView valueText;
+    }
+
+    public HashMapArrayAdapter(Context context, int textViewResourceId, List<Map.Entry<String, String>> objects) {
+        super(context, textViewResourceId, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_2, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.keyText = convertView.findViewById(android.R.id.text1);
+            viewHolder.valueText = convertView.findViewById(android.R.id.text2);
+            convertView.setTag(viewHolder);
+        } else
+            viewHolder = (ViewHolder) convertView.getTag();
+
+        Map.Entry<String, String> entry = (Map.Entry<String, String>) this.getItem(position);
+
+        viewHolder.keyText.setText(entry.getKey());
+        viewHolder.valueText.setText(entry.getValue().toString());
+        return convertView;
+    }
+}
