3c9994adfa5f373ee939238bd05b8b4c66182d91
DiogoFSVilela <diogofsvilela@gmail.com>
2019-04-03 16:24:13 +0100

    Add class for handling read and writes of sessionID

---
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/Helpers/SessionID.java
@@ -0,0 +1,28 @@
+package MobileAndUbiquitousComputing.P2Photos.Helpers;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import MobileAndUbiquitousComputing.P2Photos.R;
+
+public class SessionID {
+
+    public String getSessionID(Activity activity) {
+        SharedPreferences sharedPref = activity.getSharedPreferences(
+                activity.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        return sharedPref.getString(activity.getString(R.string.session_id_key), null);
+    }
+
+    public static void updateSessionID(Activity activity, String newSessionID) {
+        SharedPreferences sharedPref = activity.getSharedPreferences(
+                activity.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(activity.getString(R.string.session_id_key), newSessionID);
+        editor.apply(); //TODO: Change to editor.commit() if this will be called async
+    }
+
+    public static void deleteSessionID(Activity activity) {
+        updateSessionID(activity,null);
+    }
+}
