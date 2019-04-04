7e1966af1ceff5362b07b3e013990033edcdb618
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-04-04 17:37:10 +0100

    Compacted Signup logic

--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/SignUpActivity.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/SignUpActivity.java
@@ -5,14 +5,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+
+import MobileAndUbiquitousComputing.P2Photos.DataObjects.PostRequestData;
+import MobileAndUbiquitousComputing.P2Photos.DataObjects.RequestData;
+import MobileAndUbiquitousComputing.P2Photos.DataObjects.ResponseData;
 import MobileAndUbiquitousComputing.P2Photos.Exceptions.FailedLoginException;
+import MobileAndUbiquitousComputing.P2Photos.Exceptions.FailedSignupException;
 import MobileAndUbiquitousComputing.P2Photos.Exceptions.UsernameExistsException;
-import MobileAndUbiquitousComputing.P2Photos.Helpers.Signup;
+import MobileAndUbiquitousComputing.P2Photos.Exceptions.WrongCredentialsException;
+import MobileAndUbiquitousComputing.P2Photos.Helpers.ConnectionManager;
+import MobileAndUbiquitousComputing.P2Photos.Helpers.Login;
+import MobileAndUbiquitousComputing.P2Photos.Helpers.QueryManager;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -77,7 +90,7 @@ public class SignUpActivity extends AppCompatActivity {
         }
 
         try {
-            Signup.SignupUser(this, username, password);
+            signup(username, password);
             Intent intent = new Intent(this, MainMenuActivity.class);
             startActivity(intent);
         }
@@ -88,6 +101,44 @@ public class SignUpActivity extends AppCompatActivity {
         catch (FailedLoginException flex) {
             Toast toast = Toast.makeText(this, "The Login operation failed. Try again later", Toast.LENGTH_LONG);
             toast.show();
+        }
+    }
+
+    public void signup(String username, String password)
+            throws FailedSignupException, FailedLoginException, UsernameExistsException {
+        Log.i("MSG", "Signup: " + username);
+        String url = ConnectionManager.P2PHOTO_HOST + ConnectionManager.SIGNUP_OPERATION;
+        try {
+            JSONObject requestBody = new JSONObject();
+            requestBody.put("username", username);
+            requestBody.put("password", password);
+            RequestData requestData = new PostRequestData(this, RequestData.RequestType.SIGNUP, url, requestBody);
+
+            ResponseData result = new QueryManager().execute(requestData).get();
+            int code = result.getServerCode();
+            // TODO - Response codes are now in a ResponseEntity. //
+            if (code == 200) {
+                Log.i("STATUS", "The sign up operation was successful");
+            }
+            else if (code == 422) {
+                Log.i("STATUS", "The sign up operation was unsuccessful. The username chosen already exists.");
+                throw new UsernameExistsException();
+            }
+            else {
+                Log.i("STATUS", "The sign up operation was unsuccessful. Unknown error.");
+                throw new FailedSignupException();
+            }
+        }
+        catch (JSONException | ExecutionException | InterruptedException ex) {
+            throw new FailedSignupException(ex.getMessage());
+        }
+
+        try {
+            Login.login(this, username, password);
+        }
+        catch (WrongCredentialsException wcex) {
+            // Do nothing.
+            // SHOULD NEVER BE HERE. AS THE CREDENTIALS WERE USED WITHOUT CHANGE FOR SIGNING UP.
         }
     }
 }
