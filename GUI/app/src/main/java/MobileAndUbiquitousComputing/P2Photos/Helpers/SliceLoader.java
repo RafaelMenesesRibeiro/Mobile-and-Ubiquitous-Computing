b903b4c67b9b8dc8eb330d8701554d3a47af2c0e
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-05 16:22:23 +0100

    Create SliceLoader.java

---
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/Helpers/SliceLoader.java
@@ -0,0 +1,32 @@
+package MobileAndUbiquitousComputing.P2Photos.Helpers;
+
+import android.widget.ProgressBar;
+import android.os.Handler;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
+public class SliceLoader implements Callable<ArrayList<String>> {
+    private final String catalogId;
+    private final ProgressBar progressBar;
+    private final Handler progressBarHandler;
+    private ArrayList<String> slicesList = new ArrayList<>();
+    private static final int LOAD_COMPLETE = 100;
+
+    public SliceLoader(String catalogId, ProgressBar progressBar, Handler progressBarHandler) {
+        this.catalogId = catalogId;
+        this.progressBar = progressBar;
+        this.progressBarHandler = progressBarHandler;
+    }
+
+    @Override
+    public ArrayList<String> call() throws Exception {
+        // TODO call WebServer to retrieve slices list.
+        progressBarHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setProgress(LOAD_COMPLETE);
+            }
+        });
+        return slicesList;
+    }
+}
