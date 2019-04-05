8330d7bf8c9edc3385f418cee0e94145a894be31
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-04-05 13:28:02 +0100

    Added two Exceptions for response requests

---
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/Exceptions/UsernameException.java
@@ -0,0 +1,11 @@
+package MobileAndUbiquitousComputing.P2Photos.Exceptions;
+
+public class UsernameException extends RuntimeException {
+    public UsernameException() {
+        super();
+    }
+
+    public UsernameException(String msg) {
+        super(msg);
+    }
+}
