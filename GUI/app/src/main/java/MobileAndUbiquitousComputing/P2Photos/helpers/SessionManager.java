cbf76971848cc341088093fbfc7d5a0db76a2ece
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-16 23:10:33 +0100

    Introduced AuthState persistency methods. AuthState contains AuthToken and RefreshToken for GoogleAPI

--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/helpers/SessionManager.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/helpers/SessionManager.java
@@ -1,13 +1,23 @@
 package MobileAndUbiquitousComputing.P2Photos.helpers;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+import net.openid.appauth.AuthState;
+
+import org.json.JSONException;
 
 import MobileAndUbiquitousComputing.P2Photos.R;
 
-public class SessionManager {
+import static MobileAndUbiquitousComputing.P2Photos.helpers.AppContext.AUTH_STATE;
 
+public class SessionManager {
+    private static final String AUTH_STATE_SHARED_PREF = "AuthStatePreference";
     private static String sessionID;
     private static String userName = null;
 
@@ -54,4 +64,34 @@ public class SessionManager {
     public static void deleteUserName(Activity activity) {
         updateUserName(activity, null);
     }
+
+    @SuppressLint("ApplySharedPref")
+    public static  void persistAuthState(@NonNull AuthState authState, @NonNull Activity activity) {
+        activity.getSharedPreferences(AUTH_STATE_SHARED_PREF, Context.MODE_PRIVATE).edit()
+                .putString(AUTH_STATE, authState.jsonSerialize().toString())
+                .apply();
+    }
+
+    public static  void clearAuthState(@NonNull Activity activity) {
+        activity.getSharedPreferences(AUTH_STATE_SHARED_PREF, Context.MODE_PRIVATE)
+                .edit()
+                .remove(AUTH_STATE)
+                .apply();
+    }
+
+    @Nullable
+    public static AuthState restoreAuthState(@NonNull Activity activity) {
+        String jsonString =  activity.getSharedPreferences(AUTH_STATE_SHARED_PREF, Context.MODE_PRIVATE).getString(AUTH_STATE, null);
+        if (!TextUtils.isEmpty(jsonString)) {
+            try {
+                assert jsonString != null;
+                return AuthState.jsonDeserialize(jsonString);
+            } catch (JSONException jsonException) {
+                // should never happen
+            }
+        }
+        return null;
+    }
+
+
 }
