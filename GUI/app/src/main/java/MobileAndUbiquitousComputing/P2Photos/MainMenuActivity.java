77355e9fc52fd521132b98b9d83ca0ccbb7694b2
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-16 23:12:21 +0100

    Code in Progress

--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/MainMenuActivity.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/MainMenuActivity.java
@@ -1,58 +1,190 @@
 package MobileAndUbiquitousComputing.P2Photos;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import net.openid.appauth.AuthState;
+import net.openid.appauth.AuthorizationException;
+import net.openid.appauth.AuthorizationRequest;
+import net.openid.appauth.AuthorizationResponse;
+import net.openid.appauth.AuthorizationService;
+import net.openid.appauth.AuthorizationServiceConfiguration;
+import net.openid.appauth.TokenResponse;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
+import org.json.JSONObject;
+
 import MobileAndUbiquitousComputing.P2Photos.dataobjects.RequestData;
 import MobileAndUbiquitousComputing.P2Photos.dataobjects.ResponseData;
 import MobileAndUbiquitousComputing.P2Photos.exceptions.FailedOperationException;
+import MobileAndUbiquitousComputing.P2Photos.helpers.AppContext;
+import MobileAndUbiquitousComputing.P2Photos.helpers.AuthorizationCodes;
 import MobileAndUbiquitousComputing.P2Photos.helpers.QueryManager;
 import MobileAndUbiquitousComputing.P2Photos.helpers.SessionManager;
 
-import static com.google.android.gms.common.GooglePlayServicesUtil.getErrorDialog;
+import static MobileAndUbiquitousComputing.P2Photos.helpers.AppContext.APP_LOG_TAG;
+import static MobileAndUbiquitousComputing.P2Photos.helpers.SessionManager.persistAuthState;
+import static MobileAndUbiquitousComputing.P2Photos.helpers.SessionManager.restoreAuthState;
 
 public class MainMenuActivity extends AppCompatActivity {
-
-    private static final int GPS_REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-
-        GoogleApiAvailability apiAvailabilityInstance = GoogleApiAvailability.getInstance();
-
-        int resultCode = apiAvailabilityInstance.isGooglePlayServicesAvailable(getApplicationContext());
-        if (!(resultCode == ConnectionResult.SUCCESS)) {
-            Dialog errorDialog = apiAvailabilityInstance.getErrorDialog(
-                    this, resultCode, GPS_REQUEST_CODE, new DialogInterface.OnCancelListener() {
-                        @Override
-                        public void onCancel(DialogInterface dialog) {
-                            finish();
-                        }
-                    });
-
-            if (errorDialog != null) { errorDialog.show(); }
-        }
     }
 
     @Override
     public void onBackPressed() {
-        // Do nothing.
-        // Prevents going back to the login screen, or the previously completed
-        // activity.
+
+    }
+
+    public static class AuthorizeListener implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            // Declare the authorization and token endpoints of the OAuth server we wish to authorize with
+            AuthorizationServiceConfiguration serviceConfiguration = new AuthorizationServiceConfiguration(
+                    Uri.parse("https://accounts.google.com/o/oauth2/v2/auth"),  // auth endpoint
+                    Uri.parse("https://www.googleapis.com/oauth2/v4/token")     // token endpoint
+            );
+
+            // Describes actual authorization request, including our OAuth APP clientId and the scopes we are requesting
+            String clientId = "327056365677-stsv6tntebv1f2jj8agkcr84vrbs3llk.apps.googleusercontent.com";
+            Uri redirectUri = Uri.parse("https://127.0.0.1");                   // loopback address
+            AuthorizationRequest.Builder builder = new AuthorizationRequest.Builder(
+                    serviceConfiguration,
+                    clientId,
+                    AuthorizationCodes.RESPONSE_TYPE_CODE,
+                    redirectUri
+            );
+            builder.setScopes("profile");                                       // Declare scope here. drive.files, etc.
+            AuthorizationRequest request = builder.build();
+
+            // Ideally, there is one instance of AuthorizationService per Activity
+            AuthorizationService authorizationService = new AuthorizationService(view.getContext());
+
+            // Create the PendingIntent to handle the authorization response
+            String action = "MobileAndUbiquitousComputing.P2Photos.HANDLE_AUTHORIZATION_RESPONSE";
+            Intent postAuthorizationIntent = new Intent(action);
+            PendingIntent pendingIntent = PendingIntent.getActivity(
+                    view.getContext(), request.hashCode(), postAuthorizationIntent, 0
+            );
+            // Perform authorization request
+            authorizationService.performAuthorizationRequest(request, pendingIntent);
+
+            /*
+                To add handling for the authorization response. We have to add hooks into the app in order to receive
+            the authorization response from the browser.
+
+                First we register RedirectUriReceiverActivity intent-filters in AndroidManifest.xml, inside the
+            <application> block. This registers the app to receive the OAuth2 authorization response intent from the
+            system browser on our behalf.
+
+                Then, add a new intent-filter to your <activity android:name=".MainActivity"> so AppAuth can pass the
+            authorization response to your main activity.
+
+                After updating the Android Manifest file we need to create methods to handle the received intents:
+                #onNewIntent
+                #checkIntent
+                #onStart
+            */
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        checkIntent(intent);
+    }
+
+    private void checkIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            String action = intent.getAction();
+            assert action != null;
+            switch (action) {
+                case "MobileAndUbiquitousComputing.P2Photos.HANDLE_AUTHORIZATION_RESPONSE":
+                    if (!intent.hasExtra(AppContext.USED_INTENT)) {
+                        handleAuthorizationResponse(intent);
+                        intent.putExtra(AppContext.USED_INTENT, true);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    /*
+     * AppAuth provides the AuthorizationResponse to this activity, via the provided RedirectUriReceiverActivity.
+     * From it we can ultimately obtain a TokenResponse which we can use to make calls to the API;
+     */
+    private void handleAuthorizationResponse(@NonNull Intent intent) {
+        // Obtain the AuthorizationResponse from the Intent
+        AuthorizationResponse response = AuthorizationResponse.fromIntent(intent);
+        AuthorizationException error = AuthorizationException.fromIntent(intent);
+        // The AuthState object created here is a convenient way to store details from the authorization session.
+        // We can update it with the results of new OAuth responses
+        // We can also persist it to store the authorization session between app starts.
+        final AuthState authState = new AuthState(response, error);
+
+        // Exchange authorization code for the refresh and access tokens, and update the AuthState instance
+        if (response != null) {
+            Log.i(APP_LOG_TAG, "Handled Authorization Response " + authState.jsonSerialize().toString());
+            AuthorizationService service = new AuthorizationService(this);
+            service.performTokenRequest(response.createTokenExchangeRequest(), new AuthorizationService.TokenResponseCallback() {
+                @Override
+                public void onTokenRequestCompleted(@Nullable TokenResponse token, @Nullable AuthorizationException exc) {
+                    if (exc != null) {
+                        Log.w(APP_LOG_TAG, "Token Exchange failed", exc);
+                    } else {
+                        if (token != null) {
+                            authState.update(token, exc);
+                            persistAuthState(authState, MainMenuActivity.this);
+                            enablePostAuthorizationFlows();
+                            Log.i(APP_LOG_TAG, "Token Response [ Access Token: " + token.accessToken + ", ID Token: " + token.idToken + " ]");
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    private void enablePostAuthorizationFlows() {
+        // TODO
+        /*
+        mAuthState = restoreAuthState(MainMenuActivity.this);
+        if (mAuthState != null && mAuthState.isAuthorized()) {
+            if (mMakeApiCall.getVisibility() == View.GONE) {
+                mMakeApiCall.setVisibility(View.VISIBLE);
+                mMakeApiCall.setOnClickListener(new MakeApiCallListener(this, mAuthState, new AuthorizationService(this)));
+            }
+            if (mSignOut.getVisibility() == View.GONE) {
+                mSignOut.setVisibility(View.VISIBLE);
+                mSignOut.setOnClickListener(new SignOutListener(this));
+            }
+        } else {
+            mMakeApiCall.setVisibility(View.GONE);
+            mSignOut.setVisibility(View.GONE);
+        }
+        */
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkIntent(getIntent());
     }
 
     public void viewAlbumClicked(View view) {
--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/helpers/AppContext.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/helpers/AppContext.java
@@ -4,7 +4,11 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
 
+@SuppressLint("Registered")
 public class AppContext extends Application {
+    public static final String USED_INTENT = "USED_INTENT";
+    public static final String APP_LOG_TAG = "P2PHOTO MESSAGE";
+
     @SuppressLint("StaticFieldLeak")
     private static volatile Context context;
 
