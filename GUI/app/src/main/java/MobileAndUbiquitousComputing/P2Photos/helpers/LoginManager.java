f7d0789a3123cf30a9113d2c7f3fed61c2fffab4
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-16 23:34:52 +0100

    Update LoginManager.java

--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/helpers/LoginManager.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/helpers/LoginManager.java
@@ -21,29 +21,25 @@ public class LoginManager {
     public static void login(Activity activity, String username, String password) throws FailedLoginException {
         Log.i("MSG", "LoginManager: " + username);
 
-        String url =
-                activity.getString(R.string.p2photo_host) + activity.getString(R.string.login_operation);
+        String url = activity.getString(R.string.p2photo_host) + activity.getString(R.string.login_operation);
+
         try {
             JSONObject requestBody = new JSONObject();
             requestBody.put("username", username);
             requestBody.put("password", password);
-            RequestData requestData = new PostRequestData(activity, RequestData.RequestType.LOGIN
-                    , url, requestBody);
+            RequestData requestData = new PostRequestData(activity, RequestData.RequestType.LOGIN, url, requestBody);
 
             ResponseData result = new QueryManager().execute(requestData).get();
             int code = result.getServerCode();
             if (code == HttpURLConnection.HTTP_OK) {
                 Log.i("STATUS", "The login operation was successful");
-
                 SessionManager.updateUserName(activity, username);
                 // The SessionID cookie is stored in getJSONStringFromHttpResponse //
             } else if (code == HttpURLConnection.HTTP_UNAUTHORIZED) {
-                Log.i("STATUS", "The login operation was unsuccessful. The username or password " +
-                        "are incorrect.");
+                Log.i("STATUS", "The login operation was unsuccessful. The username or password " + "are incorrect.");
                 throw new WrongCredentialsException();
             } else {
-                Log.i("STATUS",
-                        "The login operation was unsuccessful. Server response code: " + code);
+                Log.i("STATUS","The login operation was unsuccessful. Server response code: " + code);
                 throw new FailedLoginException();
             }
         } catch (JSONException | ExecutionException | InterruptedException ex) {
