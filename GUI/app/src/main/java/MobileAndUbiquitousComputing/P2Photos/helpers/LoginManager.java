a1f55ddc34af71458d008d4cd5d19760b324d152
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-17 16:26:23 +0100

    Refactor code

--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/helpers/LoginManager.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/helpers/LoginManager.java
@@ -18,8 +18,10 @@ import MobileAndUbiquitousComputing.P2Photos.exceptions.WrongCredentialsExceptio
 
 public class LoginManager {
 
+    private static final String LOGIN_MGR_TAG = "LOGIN MGR";
+
     public static void login(Activity activity, String username, String password) throws FailedLoginException {
-        Log.i("MSG", "LoginManager: " + username);
+        Log.i(LOGIN_MGR_TAG, "LoginManager: " + username);
 
         String url = activity.getString(R.string.p2photo_host) + activity.getString(R.string.login_operation);
 
@@ -28,18 +30,16 @@ public class LoginManager {
             requestBody.put("username", username);
             requestBody.put("password", password);
             RequestData requestData = new PostRequestData(activity, RequestData.RequestType.LOGIN, url, requestBody);
-
             ResponseData result = new QueryManager().execute(requestData).get();
             int code = result.getServerCode();
             if (code == HttpURLConnection.HTTP_OK) {
-                Log.i("STATUS", "The login operation was successful");
+                Log.i(LOGIN_MGR_TAG, "The login operation succeded");
                 SessionManager.updateUserName(activity, username);
-                // The SessionID cookie is stored in getJSONStringFromHttpResponse //
             } else if (code == HttpURLConnection.HTTP_UNAUTHORIZED) {
-                Log.i("STATUS", "The login operation was unsuccessful. The username or password " + "are incorrect.");
+                Log.i(LOGIN_MGR_TAG, "The login operation failed. The username or password are incorrect.");
                 throw new WrongCredentialsException();
             } else {
-                Log.i("STATUS","The login operation was unsuccessful. Server response code: " + code);
+                Log.i(LOGIN_MGR_TAG,"The login operation failed. Server error with response code: " + code);
                 throw new FailedLoginException();
             }
         } catch (JSONException | ExecutionException | InterruptedException ex) {
