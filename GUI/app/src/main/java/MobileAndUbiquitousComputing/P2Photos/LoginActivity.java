8ad3d8c48a99658157d17670a8d5a40f7ecda124
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-17 17:31:50 +0100

    Update LoginActivity.java

--- GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/LoginActivity.java
+++ GUI/app/src/main/java/MobileAndUbiquitousComputing/P2Photos/LoginActivity.java
@@ -21,12 +21,15 @@ import MobileAndUbiquitousComputing.P2Photos.exceptions.WrongCredentialsExceptio
 import MobileAndUbiquitousComputing.P2Photos.helpers.AuthorizationCodes;
 import MobileAndUbiquitousComputing.P2Photos.helpers.LoginManager;
 
+import static MobileAndUbiquitousComputing.P2Photos.helpers.LoginManager.tryLogin;
+import static MobileAndUbiquitousComputing.P2Photos.helpers.LoginManager.validateGooglelAPIAuthorization;
 import static MobileAndUbiquitousComputing.P2Photos.helpers.SessionManager.AUTH_ENDPOINT;
 import static MobileAndUbiquitousComputing.P2Photos.helpers.SessionManager.TOKEN_ENDPOINT;
 import static android.widget.Toast.LENGTH_LONG;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String LOGIN_TAG = "LOGIN";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,7 +74,7 @@ public class LoginActivity extends AppCompatActivity {
         // Do nothing. Prevents going back after logging out.
     }
 
-    public void SignUpClicked(View view) {
+    public void onSignUpPressed(View view) {
         EditText usernameEditText = findViewById(R.id.usernameInputBox);
         EditText passwordEditText = findViewById(R.id.passwordInputBox);
 
@@ -85,7 +88,7 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void LoginClicked(View view) {
+    public void onLoginPressed(View view) {
         EditText usernameEditText = findViewById(R.id.usernameInputBox);
         EditText passwordEditText = findViewById(R.id.passwordInputBox);
 
@@ -93,7 +96,7 @@ public class LoginActivity extends AppCompatActivity {
         String passwordValue = passwordEditText.getText().toString().trim();
 
         try {
-            LoginManager.login(this, usernameValue, passwordValue);
+            tryLogin(this, usernameValue, passwordValue);
             Intent intent = new Intent(this, MainMenuActivity.class);
             startActivity(intent);
         } catch (WrongCredentialsException wc) {
@@ -102,54 +105,7 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(this, "Login operation failed...", LENGTH_LONG).show();
         }
 
-        tryObtainOAuthTokens();
-    }
-
-    private void tryObtainOAuthTokens() {
-        // Declare the authorization and token endpoints of the OAuth server we wish to authorize with
-        AuthorizationServiceConfiguration serviceConfiguration =
-                new AuthorizationServiceConfiguration(Uri.parse(AUTH_ENDPOINT), Uri.parse(TOKEN_ENDPOINT), null);
-
-        // Describes actual authorization request, including our OAuth APP clientId and the scopes we are requesting
-        String clientId = "327056365677-stsv6tntebv1f2jj8agkcr84vrbs3llk.apps.googleusercontent.com";
-        Uri redirectUri = Uri.parse("https://127.0.0.1");                   // loopback address
-        AuthorizationRequest.Builder builder = new AuthorizationRequest.Builder(
-                serviceConfiguration,
-                clientId,
-                AuthorizationCodes.RESPONSE_TYPE_CODE,
-                redirectUri
-        );
-        builder.setScopes("profile");                                       // Declare scope here. drive.files, etc.
-        AuthorizationRequest request = builder.build();
-
-        // Ideally, there is one instance of AuthorizationService per Activity
-        AuthorizationService authorizationService = new AuthorizationService(view.getContext());
-
-        // Create the PendingIntent to handle the authorization response
-        String action = "MobileAndUbiquitousComputing.P2Photos.HANDLE_AUTHORIZATION_RESPONSE";
-        Intent postAuthorizationIntent = new Intent(action);
-        PendingIntent pendingIntent = PendingIntent.getActivity(
-                view.getContext(), request.hashCode(), postAuthorizationIntent, 0
-        );
-        // Perform authorization request
-        authorizationService.performAuthorizationRequest(request, pendingIntent);
-
-            /*
-                To add handling for the authorization response. We have to add hooks into the app in order to receive
-            the authorization response from the browser.
-
-                First we register RedirectUriReceiverActivity intent-filters in AndroidManifest.xml, inside the
-            <application> block. This registers the app to receive the OAuth2 authorization response intent from the
-            system browser on our behalf.
-
-                Then, add a new intent-filter to your <activity android:name=".MainActivity"> so AppAuth can pass the
-            authorization response to your main activity.
-
-                After updating the Android Manifest file we need to create methods to handle the received intents:
-                #onNewIntent
-                #checkIntent
-                #onStart
-            */
+        validateGooglelAPIAuthorization(view);
     }
 
     /**********************************************************
