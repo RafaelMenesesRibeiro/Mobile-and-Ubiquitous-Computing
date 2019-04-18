6d7c3e685cad7be96a4144fb4f2f1424c1a00f10
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-18 22:21:12 +0100

    Still trying to understand the problem

--- GUI/app/src/main/java/cmov1819/p2photo/helpers/AuthStateManager.java
+++ GUI/app/src/main/java/cmov1819/p2photo/helpers/AuthStateManager.java
@@ -1,13 +1,10 @@
 package cmov1819.p2photo.helpers;
 
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -26,7 +23,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import static android.widget.Toast.LENGTH_LONG;
-import static cmov1819.p2photo.helpers.AppContext.getAppContext;
+import static cmov1819.p2photo.MainApplication.getAppContext;
 
 public class AuthStateManager {
     private static AuthStateManager instance;
