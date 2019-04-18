7bf048d7a0a4e5aeddc8a3975a381b291882f60b
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-04-18 12:24:25 +0100

    Fixed compilation errors

--- GUI/app/src/main/java/cmov1819/p2photo/AddPhotosFragment.java
+++ GUI/app/src/main/java/cmov1819/p2photo/AddPhotosFragment.java
@@ -14,6 +14,7 @@ public class AddPhotosFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_photos, container, false);
+        // return inflater.inflate(R.layout.fragment_add_photos, container, false);
+        return null;
     }
 }
--- GUI/app/src/main/java/cmov1819/p2photo/SearchUserFragment.java
+++ GUI/app/src/main/java/cmov1819/p2photo/SearchUserFragment.java
@@ -14,6 +14,7 @@ public class SearchUserFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search_user, container, false);
+        // return inflater.inflate(R.layout.fragment_search_user, container, false);
+        return null;
     }
 }
