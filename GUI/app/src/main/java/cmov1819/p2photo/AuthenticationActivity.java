27f3211dbf709e211128c5c9bc294a678eebfde7
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-04-18 23:59:57 +0100

    desperating

---
+++ GUI/app/src/main/java/cmov1819/p2photo/AuthenticationActivity.java
@@ -0,0 +1,13 @@
+package cmov1819.p2photo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
