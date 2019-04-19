289120244dc229003587f0ab644279e715f78b5a
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-04-19 16:09:16 +0100

    Implemented Add Photos Fragment

--- GUI/app/src/main/java/cmov1819/p2photo/AddPhotosFragment.java
+++ GUI/app/src/main/java/cmov1819/p2photo/AddPhotosFragment.java
@@ -1,5 +1,10 @@
 package cmov1819.p2photo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,13 +12,60 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
-import cmov1819.p2photo.R;
+import java.io.FileNotFoundException;
 
 public class AddPhotosFragment extends Fragment {
+    private View view;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_photos, container, false);
+        final View view = inflater.inflate(R.layout.fragment_add_photos, container, false);
+        this.view = view;
+        Button chooseButton = view.findViewById(R.id.choosePhoto);
+        chooseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePhotoClicked(view);
+            }
+        });
+        Button done = view.findViewById(R.id.done);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addPhotosClicked(view);
+            }
+        });
+        return view;
+    }
+
+    public void choosePhotoClicked(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            Uri targetUri = data.getData();
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(targetUri));
+                ImageView imageView = this.view.findViewById(R.id.imageView);
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void addPhotosClicked(View view) {
+        // TODO - @FranciscoBarros //
     }
 }
