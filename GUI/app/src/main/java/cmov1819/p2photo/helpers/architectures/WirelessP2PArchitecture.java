65576840200f798ff0eb61102f73f576dcc1c504
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-05-06 22:24:06 +0100

    Update WirelessP2PArchitecture.java

--- GUI/app/src/main/java/cmov1819/p2photo/helpers/architectures/WirelessP2PArchitecture.java
+++ GUI/app/src/main/java/cmov1819/p2photo/helpers/architectures/WirelessP2PArchitecture.java
@@ -7,15 +7,23 @@ import android.graphics.BitmapFactory;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import cmov1819.p2photo.LoginActivity;
@@ -26,6 +34,7 @@ import cmov1819.p2photo.exceptions.FailedOperationException;
 import cmov1819.p2photo.helpers.managers.LogManager;
 import cmov1819.p2photo.helpers.managers.SessionManager;
 
+import static cmov1819.p2photo.helpers.ConvertUtils.inputStreamToBitmap;
 import static cmov1819.p2photo.helpers.ConvertUtils.inputStreamToString;
 
 public class WirelessP2PArchitecture extends BaseArchitecture {
@@ -92,7 +101,11 @@ public class WirelessP2PArchitecture extends BaseArchitecture {
         LogManager.logViewCatalog(catalogID, catalogTitle);
     }
 
-    public void updateCatalogFile(Activity activity, String catalogId, String owner, String photoId) {
+    public void updateCatalogFile(final Activity activity,
+                                  final String catalogId,
+                                  final String owner,
+                                  final String photoId) {
+
         // Retrieve catalog file contents as a JSON Object and compare them to the received catalog file
         try {
             // Load contents
@@ -111,4 +124,67 @@ public class WirelessP2PArchitecture extends BaseArchitecture {
             LogManager.toast(activity, "Failed to add photo to catalog");
         }
     }
+
+    public List<Bitmap> getBitmapsFromFileStorage(final Activity activity,
+                                                  final String catalogId) {
+        // Retrieve catalog file contents as a JSON Object and compare them to the received catalog file
+        try {
+            // Load contents
+            String fileName = String.format("catalog_%s.json", catalogId);
+            InputStream inputStream = activity.openFileInput(fileName);
+            JSONObject catalogFileContents = new JSONObject(inputStreamToString(inputStream));
+            // Append photoId to the user photoId arrays under memberPhotos dictionary
+            JSONObject membersPhotosMap = catalogFileContents.getJSONObject("membersPhotos");
+            return loadMapPhotos(activity, membersPhotosMap);
+        } catch (IOException | JSONException exc) {
+            LogManager.logError(LogManager.NEW_CATALOG_TAG, exc.getMessage());
+            LogManager.toast(activity, "Failed to add photo to catalog");
+            return new ArrayList<>();
+        }
+    }
+
+    private List<Bitmap> loadMapPhotos(final Activity activity,
+                                       final JSONObject membersPhotosMap) {
+
+        List<Bitmap> loadedPhotos = new ArrayList<>();
+        Map<String, String> photosNotFoundMap = new HashMap<>();
+
+        Iterator<String> receivedMembers = membersPhotosMap.keys();
+        while (receivedMembers.hasNext()) {
+            String fileName = "";
+            String currentMember = receivedMembers.next();
+            try {
+                JSONArray currentMemberPhotos = membersPhotosMap.getJSONArray(currentMember);
+                for (int photoIdx = 0; photoIdx < currentMemberPhotos.length(); photoIdx++) {
+                    fileName = currentMemberPhotos.getString(photoIdx);
+                    try {
+                        Bitmap loadedPhoto = loadPhoto(activity, fileName);
+                        loadedPhotos.add(loadedPhoto);
+                    } catch (FileNotFoundException fnfe) {
+                        photosNotFoundMap.put(currentMember, fileName);
+                    }
+                }
+            } catch (JSONException jsone) {
+                continue;
+            }
+        }
+        loadedPhotos.addAll(requestMissingPhotosToPeers(photosNotFoundMap));
+        return loadedPhotos;
+    }
+
+
+    private Bitmap loadPhoto(final Activity activity, final String fileName) throws FileNotFoundException {
+        InputStream inputStream = activity.openFileInput(fileName);
+        try {
+            return inputStreamToBitmap(inputStream);
+        } catch (IOException ieo) {
+            throw new FileNotFoundException("IOException when converting inputStream to bitmap");
+        }
+    }
+
+    private List<Bitmap> requestMissingPhotosToPeers(Map<String, String> photoNotFoundList) {
+        // TODO for each fileName in list, request photo, convert it to bit map, append to list and store it;
+        return new ArrayList<>();
+    }
+
 }
