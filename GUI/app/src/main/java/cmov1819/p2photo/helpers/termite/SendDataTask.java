892999901cb70871ea13ef4d99418961d656c404
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-05-09 16:03:49 +0100

    Added logging to SendDataTask

--- GUI/app/src/main/java/cmov1819/p2photo/helpers/termite/SendDataTask.java
+++ GUI/app/src/main/java/cmov1819/p2photo/helpers/termite/SendDataTask.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import cmov1819.p2photo.R;
+import cmov1819.p2photo.helpers.managers.LogManager;
 import pt.inesc.termite.wifidirect.SimWifiP2pDevice;
 import pt.inesc.termite.wifidirect.sockets.SimWifiP2pSocket;
 
@@ -17,25 +18,28 @@ public class SendDataTask extends AsyncTask<Object, String, Void> {
 
     @Override
     protected void onPreExecute() {
-        // TODO if pre processing is required
+        LogManager.logInfo(SEND_DATA_TASK_TAG, "Started new SendData task...");
     }
 
     @Override
     protected Void doInBackground(final Object... params) {
-        P2PhotoWiFiDirectManager wiFiDirectManager = (P2PhotoWiFiDirectManager) params[0];
-
+        // P2PhotoWiFiDirectManager wiFiDirectManager = (P2PhotoWiFiDirectManager) params[0];
         SimWifiP2pDevice targetDevice = (SimWifiP2pDevice) params[1];
         byte[] data = (byte[]) params[2];
 
         try {
             // Construct a new clientSocket
             SimWifiP2pSocket clientSocket = new SimWifiP2pSocket(targetDevice.getVirtIp(), R.string.termite_port);
+            LogManager.logInfo(SEND_DATA_TASK_TAG, "Successfully created a client socket");
             // Send data to target device
             clientSocket.getOutputStream().write(data);
+            LogManager.logInfo(SEND_DATA_TASK_TAG, "Successfully written data to client socket");
             // Read any reply from target device
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             bufferedReader.readLine();
+            // End transmission
             clientSocket.close();
+            LogManager.logInfo(SEND_DATA_TASK_TAG, "Closed client socket... Operation completed");
         } catch (IOException ioe) {
             Log.e(SEND_DATA_TASK_TAG, "Error: " + ioe.getMessage());
         }
@@ -44,6 +48,6 @@ public class SendDataTask extends AsyncTask<Object, String, Void> {
 
     @Override
     protected void onPostExecute(Void result) {
-        // TODO investigate if we want to do something after doInBackground ends
+        LogManager.logInfo(SEND_DATA_TASK_TAG, "SendData task finished successfully...");
     }
 }
