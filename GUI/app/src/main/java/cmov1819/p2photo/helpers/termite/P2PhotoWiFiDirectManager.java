70832cae44339343a930780e2e3d59d1eedb2f0c
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-05-16 00:15:31 +0100

    Removed unused import

--- GUI/app/src/main/java/cmov1819/p2photo/helpers/termite/P2PhotoWiFiDirectManager.java
+++ GUI/app/src/main/java/cmov1819/p2photo/helpers/termite/P2PhotoWiFiDirectManager.java
@@ -8,7 +8,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
-import java.security.SignatureException;
 import java.util.List;
 import java.util.UUID;
 
