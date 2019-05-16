1d14aa384c250087ca9c047730c2ee28568eb10f
DiogoFSVilela <diogofsvilela@gmail.com>
2019-05-16 22:54:35 +0100

    Add configs for me

--- Termite-Cli/etc/platform/linux/env_setup.sh
+++ Termite-Cli/etc/platform/linux/env_setup.sh
@@ -3,7 +3,7 @@
 #
 # Absolute path to the Termite CLI tool
 #
-export TERMITE_CLI_PATH=/afs/.ist.utl.pt/users/1/0/ist426010/Desktop/Mobile-and-Ubiquitous-Computing/Termite-Cli
+export TERMITE_CLI_PATH=/home/lostarchie/Documents/Projects/CMU/Mobile-and-Ubiquitous-Computing/Termite-Cli
 
 #
 # Target platform; one of: mac, linux, or windows
