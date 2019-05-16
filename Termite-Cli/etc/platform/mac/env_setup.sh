05596d42747871e5613637e8bcb4f1c79fbf9305
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2019-05-16 17:26:30 +0100

    Add TermiteCLI to project

---
+++ Termite-Cli/etc/platform/mac/env_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#
+# Absolute path to the Termite CLI tool
+#
+export TERMITE_CLI_PATH=/Users/nsantos/Documents/bitbucket/project-termite/CMU1516/trunk/TermiteCMov/Termite-Cli
+
+#
+# Target platform; one of: mac, linux, or windows
+#
+export TERMITE_PLATFORM=mac
