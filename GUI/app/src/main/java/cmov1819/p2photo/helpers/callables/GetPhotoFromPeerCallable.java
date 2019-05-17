fa1751799e021776480ad9783f0c4df7cc61cfea
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2019-05-17 01:15:53 +0100

    Update GetPhotoFromPeerCallable.java

--- GUI/app/src/main/java/cmov1819/p2photo/helpers/callables/GetPhotoFromPeerCallable.java
+++ GUI/app/src/main/java/cmov1819/p2photo/helpers/callables/GetPhotoFromPeerCallable.java
@@ -4,15 +4,15 @@ import org.json.JSONObject;
 import java.util.concurrent.Callable;
 import pt.inesc.termite.wifidirect.SimWifiP2pDevice;
 
-public class RequestPhotoToPeer implements Callable<JSONObject> {
+public class GetPhotoFromPeerCallable implements Callable<JSONObject> {
     private SimWifiP2pDevice device;
 
-    public RequestPhotoToPeer(SimWifiP2pDevice device) {
+    public GetPhotoFromPeerCallable(SimWifiP2pDevice device) {
         this.device = device;
     }
 
     @Override
     public JSONObject call() throws Exception {
-        return (JSONObject) getResponseMessage(connection, Expect.GOOD_STATE_RESPONSE);
+        return null;
     }
 }
